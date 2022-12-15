a6d4dd777d59f4b8d6292cc842d53a834f822398
Parker Brandt <parker.brandt101@gmail.com>
2022-12-15 01:59:30 -0600

    Updated create_avgimage to use ImageReader classes

--- code/image_analysis.py
+++ code/image_analysis.py
@@ -1,11 +1,20 @@
 # Imports
-from skimage import io
+import numpy as np
 
-def create_avgimage(files):
+from skimage import io
+from image_parser import ImageReaderGlobal, ImageReaderCSV
+
+
+"""
+Takes in a set of images as files, and then sums up their values to create an average image
+"""
+def create_avgimage(files, configs):
     average_image=np.zeros((320, 479, 320))
-    count=0
+    imreader = ImageReaderGlobal()
+    if configs["image_out_format"] == "csv":
+        imreader = ImageReaderCSV(configs)
+
     for file in files:
-        average_image+=io.imread(file)[:,:,:320,0]
-        print("%d/%d"%(count, len(files)),end="\r")
-        count+=1
+        average_image+=imreader.io_read(file)[:,:,:320,0]
+
     return average_image
